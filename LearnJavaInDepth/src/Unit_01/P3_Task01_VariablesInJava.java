03c08fac99af6a1b7e3cad8bbbe1385489497753
Ankita Singh <Ankita Singh@LAPTOP-4THBDVCG>
2022-03-26 18:04:44 +0530

    First commit

---
+++ LearnJavaInDepth/src/Unit_01/P3_Task01_VariablesInJava.java
@@ -0,0 +1,12 @@
+package Unit_01;
+
+public class P3_Task01_VariablesInJava {
+    static int b=20;       //instance variable
+	public static void main(String[] args) {
+		
+		int a=10;    //local variable
+ System.out.println(a);
+ System.out.println(b);
+	}
+
+}
